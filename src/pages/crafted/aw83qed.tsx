dad99ecaa4bf9be93dac527a9350260e35129251
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-09-06 23:35:42 +0700

    feat: embed ppt

---
+++ src/pages/crafted/aw83qed.tsx
@@ -0,0 +1,19 @@
+import { tw } from 'twind';
+import Page from '@/components/page';
+
+export default function Aw83qed() {
+  return (
+    <Page>
+      <main className={tw(`lg:py-28 pt-28 overflow-hidden`)}>
+        <iframe
+          className={tw(`max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 bg-white`)}
+          title="crafting"
+          src="https://docs.google.com/presentation/d/e/2PACX-1vSkJ5oRgJ05bVEuL27BjsHiaq7QS9XGgmv_G-EiZKOCgCNw-FoSWM4YF2bZl5Ccjg/embed?start=false&loop=false&delayms=30000"
+          frameBorder="0"
+          width="1280"
+          height="720"
+        />
+      </main>
+    </Page>
+  );
+}
